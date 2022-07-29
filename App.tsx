fc774a140a79b46a9503ba6f11de922024f26422
Grandbusta <bustajay30@gmail.com>
2022-07-29 23:09:15 +0100

    app.tsx refractor

--- App.tsx
+++ App.tsx
@@ -1,33 +1,8 @@
-import React, {type PropsWithChildren} from 'react';
-import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
-import Home from './src/screens/home';
+import React from 'react';
 import Navigation from './src/navigation';
 
 function App() {
   return <Navigation />;
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  textView: {
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default App;
